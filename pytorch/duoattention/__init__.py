3387ee6cb089b4af2d25a9e8e3538cdbb103b31f
Chengxiang Qi <18630816527@163.com>
2025-10-24 16:05:00 +0800

    [Feature] Implement Pytorch Streaming Attention. (#5)
    * feat: Implement construct streaming mask.
    
    * feat: Implement streaming attention.
    
    * Update README.
    
    * chore: fix in cursor chat.

---
+++ pytorch/duoattention/__init__.py
@@ -0,0 +1,9 @@
+from .block_streaming_attention import (
+    construct_streaming_mask,
+    block_streaming_attention
+)
+
+__all__ =[
+    "construct_streaming_mask",
+    "block_streaming_attention"
+]
